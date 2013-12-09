0e60533eb665300c0473ab460b94ad98395ab4c6
Nikolay Antonov <ostinru@gmail.com>
2013-12-09 21:21:01 +0400

    Create simple Express App

---
+++ app.js
@@ -0,0 +1,36 @@
+var config = require('nconf');
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+config.argv()
+    .env()
+    .file({ file: 'config.json' });
+
+
+// all environments
+app.set('port', config.get('port'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('secret'));
+app.use(express.session({ secret: 'pew-pew'}));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(config.get('port'), function(){
+    console.log('Express server listening on port ' + config.get('port'));
+});
