ac532d29b8c69bce2761f51897ccd0e587fd5fed
Kulikov V <kulikoff90@gmail.com>
2014-03-03 21:55:28 +0400

    Добавлены модел пользователя  и отлов некоторых ошибок )

---
+++ middleware/sendHTTPError.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Outsourc on 09.02.14.
+ */
+module.exports = function (req, res, next) {
+
+    res.sendHttpError = function (error) {
+
+        res.status(error.status);
+        if (res.req.headers['x-requested-with'] == 'XMLHttpRequest') {
+            res.json(error);
+        } else {
+            res.render("error", {error: error});
+        }
+    };
+
+    next();
+
+};
