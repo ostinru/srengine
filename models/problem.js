9c3d94618650f2d2b0980b45900d3678c6fd3ec0
Nikolay Antonov <ostinru@gmail.com>
2014-04-14 23:56:49 +0400

    Change Mongoos's schema for User and Problem; change create_db.js to match new schema.
    Rewrite (work in progress) QA-functionality.

--- models/problem.js
+++ models/problem.js
@@ -1,37 +1,77 @@
 var mongoose = require('../lib/mongoose'),
-	Schema = mongoose.Schema;
+    async = require('async'),
+    Schema = mongoose.Schema;
+
+var firstPageObjectId = new mongoose.Types.ObjectId();
+var lastPageObjectId = new mongoose.Types.ObjectId();
+var globalObjectId = new mongoose.Types.ObjectId();
 
 var schema = new Schema({
-	topic: {
-		type: String,
-		unique: true,
-		required: true
-	},
-	question: {
-		type: String,
-		unique: true,
-		required: true
-	},
-	answers: [{
-		type: String,
-		required: true
-	}],
-	bonuses: [{
-		type: String,
-		required: false
-	}],
-	hints: [{
-		type: String,
-		required: false
-	}],
-    coast: [{
+    topic: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    question: {
+        type: String,
+        required: true
+    },
+    answers: [{
+        type: String,
+        required: true
+    }],
+    cost: {
         type: Number,
-        deafult: 0
-    }]
+        required: true
+    },
+    bonuses: [{
+        text: {
+            type: String,
+            required: true
+        },
+        cost: {
+            type: Number,
+            required: true
+        }
+    }],
+    hints: [{
+        text: {
+            type: String,
+            required: true
+        },
+        cost: {
+            type: Number,
+            required: true
+        }
+    }],
 });
 
 schema.methods.check = function(userAnswer) {
-	return this.answers.indexOf(userAnswer) !== -1;
+    return this.answers.indexOf(userAnswer) !== -1;
+};
+
+schema.methods.checkBonuses = function(userBonus) {
+    return this.bonuses.indexOf(userBonus) !== -1;
+};
+
+schema.statics.getProblem = function(problemId, callback) {
+    Problem.findById(problemId).exec(callback);
+};
+
+schema.statics.getGlobalProblem = function(callback) {
+    Problem.findById(globalObjectId, callback);
+};
+
+schema.statics.getFirstPageObjectId = function() {
+    return firstPageObjectId;
+};
+
+schema.statics.getLastPageObjectId = function() {
+    return lastPageObjectId;
+};
+
+schema.statics.getGlobalObjectId = function() {
+    return globalObjectId;
 };
 
 exports.Problem = mongoose.model('Problem', schema);
--- routes/root.js
+++ routes/root.js
@@ -3,85 +3,116 @@ var logger = require('lib/logger')(module);
 var Problem = require('models/problem').Problem;
 var HttpError = require('error').HttpError;
 
+var BONUS_KEY_WORD = "бонус";
+var HINT_KEY_WORD = "подсказка";
+
 exports.get = function(req, res, next){
+    if (res.req.headers['x-requested-with'] != 'XMLHttpRequest') {
+        return res.sendHttpError(new HttpError(412, "Only XMLHttpRequest requests accepted on this URL"));
+    }
+
     if (!req.user) {
-        res.sendHttpError(new HttpError(401));
-        return;
+        return res.sendHttpError(new HttpError(401));
     }
     if (!req.user.problemId) {
-        res.sendHttpError(new HttpError(500));
-        return;
+        return res.sendHttpError(new HttpError(500, 'There are no probem for current user'));
     }
 
     var problemId = req.user.problemId;
 
-    getProblem(problemId, function(err, problem) {
+    Problem.getProblem(problemId, function(err, problem) {
         if (err) {
             return res.sendHttpError(err); // TODO: is it ok?
         }
-        res.locals.problem = problem;
-        res.render('index');
+        var publicProblem = {
+            topic : problem.topic,
+            question : problem.question
+        };
+        
+        // TODO: add bonuses and hints
+        publicProblem.takenBonuses = [ "bonus 1", "bonus 2"];
+        publicProblem.takenHints = ["hint 1", "hint 2"]
+
+        res.json(publicProblem);
     });
 };
 
-exports.post = function(req, res, next){
+exports.post = function(req, res, next) {
     logger.info('POST on "' + req.path + '": ', req.body);
-    var answer = req.body.answer;
-    if (!answer) {
-        // TODO: fail
+
+    if (res.req.headers['x-requested-with'] != 'XMLHttpRequest') {
+        return res.sendHttpError(new HttpError(412, "Only XMLHttpRequest requests accepted on this URL"));
     }
+
     // 1) get user
     var user = req.user;
     if (!user) {
-        res.sendHttpError(new HttpError(401));
-        return;
+        return res.sendHttpError(new HttpError(401));
     }
-    if (!user.problemId) {
-        res.sendHttpError(new HttpError(500));
-        return;
-    }
-
     var problemId = user.problemId;
+    if (!problemId) {
+        return res.sendHttpError(new HttpError(500));
+    }
+    var answer = req.body.answer;
+    if (!answer) {
+        return res.sendHttpError(new HttpError(403, "No answer"));
+    }
 
-    // 2) get problem
-    getProblem(problemId, function (err, problem) {
+    Problem.getProblem(problemId, function (err, problem) {
         if (err) {
-            res.sendHttpError(err); // TODO: is it ok?
-            return;
+            return res.sendHttpError(err);
         }
-        // 3) check
-        logger.debug('checking answer "' + answer + '". Correct answers: ' + problem.answers + '. result is ' + problem.check(answer));
-        if (problem.check(answer)) {
-            // TODO: set next question:
-            // TODO: reload page?
-            return res.redirect('/map');
-        }
-        res.locals.problem = problem;
-        res.render('index');
+        Problem.getGlobalProblem(function(err, globalProblem) {
+            if (err) {
+                return res.sendHttpError(err);
+            }
+
+            answer = normalizeAnswer(answer);
+
+            if (answer.indexOf(BONUS_KEY_WORD) == 0) {
+                var bonusStr = answer.substring(BONUS_KEY_WORD.length + 1);
+                if (problem.checkBonuses(bonusStr)) {
+                    // TODO: write bonus to user
+                }
+                else if (globalProblem.checkBonuses(bonusStr)) {
+                    // TODO: write bonus to user
+                }
+                else {
+                    return res.sendHttpError(new HttpError("No such bonus '"+bonusStr+"'"));
+                }
+            }
+            else if (answer.indexOf(HINT_KEY_WORD) == 0) {
+                var hintNumberStr = answer.substring(HINT_KEY_WORD.length + 1);
+                var hintNumber = parseInt(hintNumberStr);
+                if (isNaN(hintNumber)) {
+                    return res.sendHttpError(new HttpError("Can't parse hint numer '" + hintNumberStr + "'"));
+                }
+                // TODO: work with only  "подсказка 2"
+                var hint = problem.bonuses[hintNumber - 1];
+                if (hint === undefined) {
+                    return res.sendHttpError(new HttpError("No such hint for this problem"));
+                }
+                // TODO: write hint to user
+                res.json({hint : hint});
+            }
+            else {
+                logger.debug('checking answer "' + answer + '". Correct answers: ' + problem.answers + '. result is ' + problem.check(answer));
+                if (problem.check(answer)) {
+                    // TODO: set next question:
+                    return res.json({}); // TODO
+                }
+                else if (globalProblem.check(answer)) {
+                    // TODO: set next question:
+                    return res.json({}); // TODO
+                }
+                else {
+                    return res.sendHttpError(new HttpError("No such answer '" + answer + "'"));
+                }
+            }
+        });
     });
 };
 
-// TODO: rename function
-function getProblem(problemId, callback) {
-    logger.debug('Looking for problemID ' + problemId);
-    async.waterfall([
-        function (callback) {
-            Problem.findOne({ _id : problemId }).exec(callback);
-        },
-        function (problem, callback) {
-            if (!problem) {
-                return callback(new HttpError(403), null);
-            }
-            callback(null, problem);
-        }
-    ],
-        function(err, problem) {
-            if (err) {
-                logger.error("GET PROBLEM:", err);
-                callback(err, null);
-                return;
-            }
-            callback(null, problem);
-        }
-    );
+normalizeAnswer = function(answer) {
+    return answer;
 }
