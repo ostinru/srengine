796403a986b0fdf2e2af3e4539799a7229ccbc38
Kulikov Valentin <kulikoff90@gmail.com>
2014-09-06 23:10:17 +0400

    Button "Add problem"

--- routes/problems.js
+++ routes/problems.js
@@ -1,6 +1,7 @@
 var logger = require('lib/logger')(module);
 var Problem = require('models/problem').Problem;
 var HttpError = require('error').HttpError;
+var mongoose = require('lib/mongoose.js');
 
 exports.get = function(req, res, next){
     Problem.find({},null,{sort: {serial: 1}}, function (err, problems) {
@@ -17,3 +18,18 @@ exports.get = function(req, res, next){
         }
     });
 };
+
+exports.post = function(req,res,next) {
+    var newProblem = new Problem({topic:'99',
+        serial:999,
+        question:'999',
+        answers:[],
+        cost:999,
+        hints:[],
+        bonuses:[]});
+    newProblem.save();
+    logger.info(newProblem._id);
+    res.locals.problem = newProblem;
+    res.location('/problem/'+newProblem._id);
+    return res.render('problem');
+}
