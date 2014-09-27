227168012a55a9aaabc239aeb627ba071a95287d
klaudgrave <armaultima7@googlemail.com>
2014-09-27 14:50:26 +0400

    Update clearHistory.js

--- clearHistory.js
+++ clearHistory.js
@@ -6,59 +6,14 @@ var HttpError = require('error').HttpError;
 var _ = require('underscore');
 var mongoose = require('lib/mongoose.js');
 
-exports.get = function(req, res, next){
-    if (!req.params.userId) {
-        return res.sendHttpError(new HttpError(404, "userId not set"));
-    }
-
-    User.findById(req.params.userId, function(err, user) {
-        if (err) {
-            return res.sendHttpError(new HttpError(404, err));
-        }
-
-        if (res.req.headers['x-requested-with'] == 'XMLHttpRequest') {
-            return res.json(user);
-        }
-        else {
-            if(!user){
-                logger.info("create new user");
-                user =  new User({username:'юзернэйм',
-                    password:'пассворд',
-                    admin:false,
-                    _id: mongoose.Types.ObjectId(req.params.userId)});
-            }
-            Problem.find(function(err,problems){
-                res.locals.countProblem = problems.length-1;//без глобального бонуса
-                res.locals.user = user;
-                return res.render("user");
-            });
-        }
-    });
-};
-
 exports.post = function(req, res, next) {
 
     User.findById(req.params.userId, function(err, user) {
         if(!user){
-            user =  new User({username:'юзернэйм',
-                password:'пассворд',
-                admin:false,
-                _id: mongoose.Types.ObjectId(req.params.userId)});
+         return false;
         }
-        user.username = req.body.username;
-        user.password = req.body.password;
-        user.admin = req.body.admin;
-        var i = 0;
-        while (!(req.body['numb' + i] === undefined)) {
-            if (i < user.problemQueue.length) {
-                user.problemQueue[i] = req.body['numb' + i];
-            }
-            else {
-                user.problemQueue.push(req.body['numb' + i]);
-            }
-            i++;
-        }
-        user.markModified('problemQueue');
+        user.problemHistory=[];
+        
         if (user.problemHistory.length === 0) {
             user.problemHistory.push({problemId: Problem.getGlobalObjectId(), solved: true});
         }
