6be3682c9ce47c46ff4e6ebfa6605d2745a3872a
Nikolay Antonov <nikolay.antonov@exactprosystems.com>
2014-10-02 16:17:12 +0400

    Get PageTitle from config.

--- middleware/resLocals.js
+++ middleware/resLocals.js
@@ -1,6 +1,8 @@
+var config = require('../config');
+
 module.exports = function(req, res, next) {
     res.locals.session = req.session;
     res.locals.user = req.user;
-
+	res.locals.pageTitle = config.get('pageTitle');
     next();
 };
