51fae8689676f133e76d1d83f986559300ec9422
Valentin <kulukoff90@gmail.com>
2015-08-22 00:53:14 +0300

    fix sendHTTPError
    post on /coords

--- middleware/sendHTTPError.js
+++ middleware/sendHTTPError.js
@@ -7,12 +7,12 @@ module.exports = function (req, res, next) {
         var proxy = req.headers['x-forwarded-for'] || '';
         var ip = (req.connection && req.connection.remoteAddress) ||
             (req.socket && req.socket.remoteAddress) ||
-            (req.connection && req.connection.socket.remoteAddress);
+            (req.connection && req.connection.socket && req.connection.socket.remoteAddress);
         
         logger.debug('[proxy="%s", IP="%s"][%s] %s: %s', proxy, ip, username, error.status, error.message);
 
         res.status(error.status);
-        if (res.req.headers['x-requested-with'] == 'XMLHttpRequest') {
+        if (req.headers['x-requested-with'] == 'XMLHttpRequest') {
             res.json(error);
         } else {
             res.render("error", {error: error});
--- routes/coords.js
+++ routes/coords.js
@@ -1,8 +1,13 @@
 var mongoose = require('lib/mongoose.js');
+var HttpError = require('error').HttpError;
 var User = require('models/user').User;
 var Coords = require('models/coords').Coords;
+var Problem = require('models/problem').Problem;
+var _ = require('underscore');
+var aDistance = require('../config').get("aDistance");
 
 exports.post = function(req,res,next) {
+    var user = req.user;
 
     var coords = new Coords({
         sessionID: req.sessionID,
@@ -17,6 +22,57 @@ exports.post = function(req,res,next) {
             console.log(err);
             return res.sendHttpError(new HttpError(500, err));
         }
-        res.json({});
+        var problem = _.find(user.problems, function(problem) {
+            var opened  = _.find(user.problemHistory, function(strProblem){
+                 return strProblem.problem._id.equals(problem._id);
+            });
+            return !opened  && inProblem(problem,coords);
+        });
+        console.log(problem);
+        if(!problem){
+            //мы еще никуда не приехали
+            res.json({status:"Success"})
+        }
+        user.problemHistory.push({
+            problem:problem._id,
+            solved:false,
+            takenBonuses:[],
+            takenHints:[],
+            timeStart:Date.now()});
+        console.log(user.problemHistory);
+        user.markModified("problemHistory");
+        user.save(function(err){
+            if (err) {
+                return res.sendHttpError(new HttpError(500, err));
+            }
+            return res.json({
+                status:"Success",
+                message:"problem " + problem.topic + " is opened",
+                reload:true
+            })
+        });
     })
+}
+
+//Функция проверки на вхождение в круг на сфере
+intersects = function(rPoint,rCircle) {
+     return rPoint-rCircle <= 0
+}
+
+//Функция расстояния в км между 2мя точками (коорд)
+arcDistance = function(loc1, loc2) {
+    var rad = Math.PI / 180,
+        earth_radius = 6371.009, // close enough
+        lat1 = loc1.lat * rad,
+        lat2 = loc2.lat * rad,
+        dlon = Math.abs(loc1.lng - loc2.lng) * rad,
+        M = Math;
+    return earth_radius * M.acos(
+            (M.sin(lat1) * M.sin(lat2)) + (M.cos(lat1) * M.cos(lat2) * M.cos(dlon))
+        );
+}
+
+inProblem = function(problem,coords){
+     deltaR = arcDistance({lat:coords.x,lng:coords.y},{lat:problem.x,lng:problem.y});
+    return intersects(deltaR, aDistance);
 }
