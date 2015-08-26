0da945a8f3e9ad4dd228362b092e8eeb708accdc
Nikolay Antonov <ostinru@gmail.com>
2015-08-26 21:33:31 +0300

    Fix checkFinished

--- middleware/checkFinished.js
+++ middleware/checkFinished.js
@@ -1,13 +1,20 @@
 var logger = require('lib/logger')(module);
 var Problem = require('models/problem').Problem;
+var _ = require('underscore');
 
 module.exports = function (req, res, next) {
-    if (req.user.problems.length === 0) {
+    var toSolve = _.reduce(req.user.problems, function(memo, problem) {
+        return problem.forHints ? memo : memo + 1;
+    }, 0);
+
+    if (toSolve === 0) {
         logger.info("Finish: nothing to solve");
         if (req.headers['x-requested-with'] == 'XMLHttpRequest') {
             return res.json({status: "Success", message: 'Finished', reload: true});
         } else {
             return res.render('finish');
         }
+    } else {
+        next();
     }
 }
